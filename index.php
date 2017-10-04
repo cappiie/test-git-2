b6145a1ee8b81a845b62c5d645ec67705a72b24e
loic <loicflament62@gmail.com>
2017-10-04 13:22:59 +0200

    Hello World

---
+++ index.php
@@ -0,0 +1,9 @@
+<html class="no-js" lang="fr" prefix="og: http://ogp.me/ns#">
+<head>
+	<link href="styles/stylesheets/screen.css" rel="stylesheet">
+<head>
+<body>
+	Hello World
+
+</body>
+</html>
